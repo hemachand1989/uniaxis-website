9cd11207377a98d4e9edd1bb58a266b193ab3457
hemachand1989 <hemachand.paruchuri@gmail.com>
2025-07-28 21:41:34 +1000

    Add TeamSection to sections index

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,5 +1,6 @@
 export { HeroSection } from './HeroSection';
 export { AboutSection } from './AboutSection';
+export { TeamSection } from './TeamSection';
 export { ServicesSection } from './ServicesSection';
 export { PortfolioSection } from './PortfolioSection';
 export { ContactSection } from './ContactSection';
